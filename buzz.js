a6d2fc776ed92f1438eb90c0b6ce086dae3519c4
tina-popova <tina.popova1@gmail.com>
2022-09-17 01:03:31 +0300

    change buzz.js

--- buzz.js
+++ buzz.js
@@ -1,2 +1,15 @@
-console.log("Test");
-console.log(2 + 5);
+let result = "";
+for (counter = 0; counter < 7; counter += 1) {
+  result += "#";
+  console.log(result, "\n");
+}
+
+let num = 1;
+var counter = 0;
+while (counter < 25) {
+  if (num % 3 == 0 && num % 5 != 0) console.log("Fizz");
+  else if (num % 3 != 0 && num % 5 == 0) console.log("Buzz");
+  else console.log(num);
+  counter += 1;
+  num += 1;
+}
