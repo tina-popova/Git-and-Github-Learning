f1d882ce8bf4e5f37c1ab9300139965ab2b5cf3f
tina-popova <tina.popova1@gmail.com>
2022-09-17 01:26:36 +0300

    feat: add function findSolution

--- buzz.js
+++ buzz.js
@@ -1,12 +1,23 @@
-let result = "";
-for (counter = 0; counter < 7; counter += 1) {
-  result += "#";
-  console.log(result, "\n");
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return (
+        find(current + 5, `(${history} + 5)`) ||
+        find(current * 3, `(${history} * 3)`)
+      );
+    }
+  }
+  return find(1, "1");
 }
+console.log(findSolution(77));
 
 let num = 1;
 var counter = 0;
-while (counter < 25) {
+while (counter < 15) {
   if (num % 3 == 0 && num % 5 != 0) console.log("Fizz");
   else if (num % 3 != 0 && num % 5 == 0) console.log("Buzz");
   else console.log(num);
